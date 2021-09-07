130c008d3bfc20a137561824737278e9a031779e
Adithya K Shetty <adithyakshetty01@gmail.com>
2021-09-07 16:09:11 +0530

    intial version of todo list

--- vue.js
+++ vue.js
@@ -2,8 +2,32 @@ let myApp = Vue.createApp({
     data() {
         return {
             userInput: '#title',
+            userKey: '',
+            myTodo: '',
+            todoArray: []
         };
     },
+    computed: {
+        inputTextFieldStyle() {
+            return {
+                hideInputTextField: this.userKey === 'Enter',
+            };
+        },
+    },
+    methods: {
+        setKeys(event) {
+            if (event.key === "Enter") {
+                this.userKey = "Enter";
+                console.log(this.userKey);
+            }
+        },
+        setTodo() {
+            this.todoArray.push(this.myTodo);
+        },
+    },
+
+
+
 
 });
 myApp.mount('#myVueTemp');
